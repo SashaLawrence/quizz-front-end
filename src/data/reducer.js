ff43fae3706fb871ed2f08ead5b4cafa828b2776
Rowley thompson <thompsonrjwt.gmail.com>
2019-09-19 11:01:09 +0100

    answer css styling validation complete

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -5,12 +5,17 @@ const start =  state => ({
     start: true,
 })
 
+const increment = state => ({
+    ...state,
+    qcounter: state.qcounter + 1,
+})
 
 
 
 const reducer = (state, action) => {
     switch (action.type) {
     case "start": return start(state, action)
+    case "next": return increment(save(state, action))
      default: return state;
      } 
  };
