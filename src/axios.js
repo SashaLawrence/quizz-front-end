4a3af5ea65b1c94f3bfcf173adc206cdd60d4a92
Jonathan Smithies <jonathan@elev8now.co.uk>
2019-09-19 10:30:55 +0100

    added database axios creds to axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,9 +3,9 @@ import axios from "axios";
 // create a version of axios with useful defaults
 export default axios.create({
     // use your own url
-    baseURL: "http://rowley.restful.training/api/ping-pong/games/",
+    baseURL: "http://homestead.test/api/quiz",
     // use your own key
-    params: {"key": "329b4a1603504df9b05cba4b9dbfe57c68e1946c"},
+    params: { "key": "bCYb3k8XuKIMOlhNrcPQ880FMg4S3XDvGEdcgcJV" },
     // make sure we get JSON back
     headers: {"Accept": "application/json"},
 });
