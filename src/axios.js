b6892f8e58c8e778928d1742dbd065e623c8cb91
Rowley thompson <thompsonrjwt.gmail.com>
2019-09-20 15:14:52 +0100

    update baseURL in axios

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // create a version of axios with useful defaults
 export default axios.create({
     // use your own url
-    baseURL: "http://homestead.test/api",
+    baseURL: "http://ec2-3-10-54-163.eu-west-2.compute.amazonaws.com/api",
     // use your own key
     params: { "key": "bCYb3k8XuKIMOlhNrcPQ880FMg4S3XDvGEdcgcJV" },
     // make sure we get JSON back
